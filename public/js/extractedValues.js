465bd427797779371cad4597fda6e0e96804f703
Rohan <sarith21@gmail.com>
2014-05-18 20:32:23 -0700

    added function to get coordinates of midpoint between users eyes

--- public/js/extractedValues.js
+++ public/js/extractedValues.js
@@ -1,1 +1,14 @@
 var extractedValues = {};
+
+//extractedValues.coords defined in sampleWorkFlow.js, since it needs access 
+//to that file's internal closure scope
+
+extractedValues.getCameraPosition = function(){
+	var eye1 = extractedValues.coords(0);
+	var eye2 = extractedValues.coords(1);
+
+	var xOut = ( (eye1[0] + eye2[0]) >> 1 );
+	var yOut = ( (eye1[1] + eye2[1]) >> 1 );
+
+	return [xOut, yOut];
+}
