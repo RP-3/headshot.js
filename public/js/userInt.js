0b9447db8ea18f1090b0e7f8ea40a96bf16ebf1f
Rohan <sarith21@gmail.com>
2014-05-20 14:38:44 -0700

    added JQ actions to change game difficulty on keyboard input

--- public/js/userInt.js
+++ public/js/userInt.js
@@ -6,7 +6,14 @@ $(document).ready(function(){
 			animate();
 		}else if(event.keyCode === 49){ //press 1 to create cubes
 			cubeMaker();
+		}else if(event.keyCode === 93){ //press up to increase difficultly and play
+			tracker.difficulty++;
+			tracker.play();
+		}else if(event.keyCode === 91){ //press down to decrease difficulty and play
+			if(tracker.difficulty > 1){
+				tracker.difficulty--;
+			}
+			tracker.play();
 		}
-
 	});
 });
