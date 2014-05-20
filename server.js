2f61cb94e43fdbae880407b6c338cfc14e4c7494
Rohan <sarith21@gmail.com>
2014-05-20 00:20:24 -0700

    correct deployment port

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
-}).listen(8080);
+}).listen(process.env.PORT || 8080);
 
-console.log('listening on port 8080')
+console.log('listening on port 8080');
